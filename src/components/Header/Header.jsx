b99561f068b2b62f89505858869780d49f7f1f6e
cmdspeed <cmdspeed@gmail.com>
2025-08-09 17:32:12 +0200

    add style icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
           aria-label="User menu"
         >
           <ul className="header__icons__list list-unstyled d-flex justify-content-end align-items-center mb-0">
-            <li className="rounded-circle border p-1">
+            <li className="rounded-circle justify-content-center border p-1">
               <div className="header__icon rounded-circle p-2">
                 <a href="#" aria-label="User profile">
                   <img src={user} alt="User-icon" />
@@ -26,24 +26,29 @@ export const Header = () => {
               </div>
             </li>
 
-            <li className="rounded-circle border p-1">
+            <li className="rounded-circle justify-content-center border p-1">
               <div className="header__icon rounded-circle p-2">
                 <a href="#" aria-label="Favorites">
                   <img src={favorite} alt="Favorites-icon" />
                 </a>
               </div>
             </li>
-            <li className="rounded-circle border p-1">
+            <li className="rounded-circle justify-content-center border p-1">
               <div className="header__icon rounded-circle px-2 py-1">
                 <a href="#" aria-label="Shopping bag">
                   <img src={bag} alt="bag-icon" />
                 </a>
               </div>
             </li>
-            <li>
-              <button aria-label="Open menu">
-                <img src={burger} alt="menu-icon" />
-              </button>
+            <li className="rounded-circle justify-content-center border p-1">
+              <div className="header__icon header__burger rounded-circle ">
+                <button
+                  aria-label="Open menu"
+                  className="border-0 bg-transparent"
+                >
+                  <img src={burger} alt="menu-icon" />
+                </button>
+              </div>
             </li>
           </ul>
         </nav>
