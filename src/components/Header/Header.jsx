333adbef7706925a2aedc8fc6cb7d3ae9905a692
cmdspeed <cmdspeed@gmail.com>
2025-08-09 23:31:33 +0200

    copy nav to new component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,8 @@
 import "../styles/header.less";
 import logo from "../../icon/logo.svg";
-import user from "../../icon/user.svg";
-import burger from "../../icon/burger.svg";
-import bag from "../../icon/bag.svg";
-import favorite from "../../icon/favorite.svg";
+
 import search from "../../icon/search.svg";
+import { Navigation } from "../Navigation/Navigation";
 
 export const Header = () => {
   return (
@@ -14,45 +12,7 @@ export const Header = () => {
           <img src={logo} alt="logo" />
         </div>
 
-        <nav
-          className="header__container__icons col-7 text-right"
-          aria-label="User menu"
-        >
-          <ul className="header__icons__list list-unstyled d-flex justify-content-end align-items-center mb-0">
-            <li className="rounded-circle justify-content-center border p-1">
-              <div className="header__icon rounded-circle p-2 d-flex align-items-center">
-                <a href="#" aria-label="User profile">
-                  <img src={user} alt="User-icon" />
-                </a>
-              </div>
-            </li>
-
-            <li className="rounded-circle justify-content-center border p-1">
-              <div className="header__icon rounded-circle p-2 d-flex align-items-center">
-                <a href="#" aria-label="Favorites">
-                  <img src={favorite} alt="Favorites-icon" />
-                </a>
-              </div>
-            </li>
-            <li className="rounded-circle justify-content-center border p-1">
-              <div className="header__icon rounded-circle px-2 py-1 d-flex align-items-center">
-                <a href="#" aria-label="Shopping bag">
-                  <img src={bag} alt="bag-icon" />
-                </a>
-              </div>
-            </li>
-            <li className="rounded-circle justify-content-center border p-1">
-              <div className="header__icon header__burger rounded-circle d-flex align-items-center">
-                <button
-                  aria-label="Open menu"
-                  className="border-0 bg-transparent"
-                >
-                  <img src={burger} alt="menu-icon" />
-                </button>
-              </div>
-            </li>
-          </ul>
-        </nav>
+        <Navigation />
 
         <form
           role="search"
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,45 @@
+import user from "../../icon/user.svg";
+import burger from "../../icon/burger.svg";
+import bag from "../../icon/bag.svg";
+import favorite from "../../icon/favorite.svg";
+
+export const Navigation = () => {
+  return (
+    <nav
+      className="header__container__icons col-7 text-right"
+      aria-label="User menu"
+    >
+      <ul className="header__icons__list list-unstyled d-flex justify-content-end align-items-center mb-0">
+        <li className="rounded-circle justify-content-center border p-1">
+          <div className="header__icon rounded-circle p-2 d-flex align-items-center">
+            <a href="#" aria-label="User profile">
+              <img src={user} alt="User-icon" />
+            </a>
+          </div>
+        </li>
+
+        <li className="rounded-circle justify-content-center border p-1">
+          <div className="header__icon rounded-circle p-2 d-flex align-items-center">
+            <a href="#" aria-label="Favorites">
+              <img src={favorite} alt="Favorites-icon" />
+            </a>
+          </div>
+        </li>
+        <li className="rounded-circle justify-content-center border p-1">
+          <div className="header__icon rounded-circle px-2 py-1 d-flex align-items-center">
+            <a href="#" aria-label="Shopping bag">
+              <img src={bag} alt="bag-icon" />
+            </a>
+          </div>
+        </li>
+        <li className="rounded-circle justify-content-center border p-1">
+          <div className="header__icon header__burger rounded-circle d-flex align-items-center">
+            <button aria-label="Open menu" className="border-0 bg-transparent">
+              <img src={burger} alt="menu-icon" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
+};
