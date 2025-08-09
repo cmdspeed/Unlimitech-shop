ea2c9beaa15a29fa3de2aa8cfaf4f77c74515907
cmdspeed <cmdspeed@gmail.com>
2025-08-09 23:08:28 +0200

    add style form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import user from "../../icon/user.svg";
 import burger from "../../icon/burger.svg";
 import bag from "../../icon/bag.svg";
 import favorite from "../../icon/favorite.svg";
+import search from "../../icon/search.svg";
 
 export const Header = () => {
   return (
@@ -52,6 +53,28 @@ export const Header = () => {
             </li>
           </ul>
         </nav>
+
+        <form
+          role="search"
+          aria-label="Wyszukiwarka"
+          className="form mx-auto d-flex align-items-center border border-1 rounded-5 overflow-hidden position-relative col-12 "
+        >
+          <input
+            id="search"
+            name="search"
+            type="search"
+            placeholder="Wyszukaj"
+            aria-label="Wyszukaj"
+            className="form__input d-flex border-0 "
+          />
+          <button
+            type="submit"
+            aria-label="Wyszukaj"
+            className="form__btn position-absolute border-0 rounded-circle d-flex align-items-center justify-content-center cursor-pointer"
+          >
+            <img src={search} alt="search icon" className="form__btn__icon" />
+          </button>
+        </form>
       </div>
     </header>
   );
