924e32f4a29179c29400c0ce3e98fd5b092a4f36
cmdspeed <cmdspeed@gmail.com>
2025-08-10 15:27:33 +0200

    slider with style

--- src/components/SlickSlider/SlickSlider.jsx
+++ src/components/SlickSlider/SlickSlider.jsx
@@ -3,21 +3,35 @@ import $ from "jquery";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel";
+import "../styles/slickSlider.less";
 
 export const SlickSlider = () => {
   const [images, setImages] = useState([]);
+  const width = 358;
+  const height = 520;
 
   useEffect(() => {
     fetch("https://picsum.photos/v2/list?limit=3")
       .then((res) => res.json())
-      .then((data) => setImages(data));
+      .then((data) => {
+        const slidesWithText = data.map((img, idx) => ({
+          id: img.id,
+          img: `https://picsum.photos/id/${img.id}/${width}/${height}`,
+          title: idx === 0 ? "Nowa kolekcja" : `Nowa kolekcja ${idx + 1}`,
+          description:
+            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab.",
+          buttonText: "Zobacz więcej",
+        }));
+        setImages(slidesWithText);
+      });
   }, []);
 
   useEffect(() => {
     if (images.length > 0) {
       setTimeout(() => {
-        $(".my-slider").not(".slick-initialized").slick({
+        $(".slider").not(".slick-initialized").slick({
           dots: true,
+          arrows: false,
           infinite: true,
           speed: 300,
           slidesToShow: 1,
@@ -34,14 +48,23 @@ export const SlickSlider = () => {
   }
 
   return (
-    <div className="my-slider">
-      {images.map((img) => (
-        <div key={img.id}>
-          <img
-            src={img.download_url}
-            alt={img.author}
-            style={{ width: "100%", height: "auto" }}
-          />
+    <div className="slider mx-auto p-0">
+      {images.map((image) => (
+        <div key={image.id} className="slider__item">
+          <div className="slider__image__wrapper position-relative overflow-hidden ">
+            <img
+              src={image.img}
+              alt={image.title}
+              className="slider__image d-block object-fit-cover rounded-5"
+            />
+            <div className="slider__overlay position-absolute">
+              <h2>{image.title}</h2>
+              <p className="fw-medium">{image.description}</p>
+              <button className="bg-transparent border border-white rounded text-white fw-bold btn">
+                {image.buttonText}
+              </button>
+            </div>
+          </div>
         </div>
       ))}
     </div>
