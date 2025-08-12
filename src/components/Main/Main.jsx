a5170d96231ef55dace135ddadbb332994cf3f79
cmdspeed <cmdspeed@gmail.com>
2025-08-12 11:28:44 +0200

    feat: add newsletter

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,5 @@
 import { Category } from "../Category/Category";
+import { Newsletter } from "../Newsletter/Newsletter";
 import { Products } from "../Product/Product";
 
 export const Main = () => {
@@ -6,6 +7,7 @@ export const Main = () => {
     <>
       <Products />
       <Category />
+      <Newsletter />
     </>
   );
 };
