5cad36a49d9f8383c3c7d10e669a43f0ca0c5c4c
cmdspeed <cmdspeed@gmail.com>
2025-08-14 11:13:42 +0200

    feat: add style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,96 +7,207 @@ import email from "../../icon/email.svg";
 
 export const Footer = () => {
   return (
-    <footer>
-      <div>
-        <div>
-          <div>
-            <div>
-              <h6>O nas</h6>
+    <footer className="footer rounded-4 ">
+      <div className="footer__container">
+        <div className="row">
+          <div className="footer__wrapper col-12 col-md-2  mobile-wrapper ">
+            <div className="footer__wrapper__border border-bottom border-1 ">
+              <h6 className="footer__wrapper__title fw-bold position-relative d-flex align-items-center justify-content-between">
+                O nas
+                <img
+                  src={arrowdrop}
+                  alt="rozwiń menu"
+                  className="footer__wrapper__arrow"
+                />
+              </h6>
 
-              <ul>
+              <ul className="footer__wrapper__list list-unstyled">
                 <li>
-                  <a href="/">Poznajmy się</a>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Poznajmy się
+                  </a>
                 </li>
                 <li>
-                  <a href="/">Skontaktuj się z nami</a>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Skontaktuj się z nami
+                  </a>
                 </li>
                 <li>
-                  <a href="/">Dołącz do nas</a>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Dołącz do nas
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div>
-            <div>
-              <h6>Informacje</h6>
-              <ul>
+          <div className="footer__wrapper col-12 col-md-2  mobile-wrapper ">
+            <div className="footer__wrapper__border border-bottom border-1 ">
+              <h6 className="footer__wrapper__title fw-bold position-relative d-flex align-items-center justify-content-between">
+                Informacje
+                <img
+                  src={arrowdrop}
+                  alt="rozwiń menu"
+                  className="footer__wrapper__arrow"
+                />
+              </h6>
+              <ul className="footer__wrapper__list list-unstyled">
                 <li>
-                  <a href="/">Regulamin</a>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Regulamin
+                  </a>
                 </li>
                 <li>
-                  <a href="/">Polityka prywatności</a>
+                  <a
+                    href="/"
+                    className="footer__wrapper__list__item text-decoration-none "
+                  >
+                    Polityka prywatności
+                  </a>
                 </li>
                 <li>
-                  <a href="/">Zasady gwarancji</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <h6>Twoje konto</h6>
-              <ul>
-                <li>
-                  <a href="/">Twoje konto</a>
-                </li>
-                <li>
-                  <a href="/">Twoje zamówienia</a>
-                </li>
-                <li>
-                  <a href="/">Logowanie</a>
-                </li>
-                <li>
-                  <a href="/">Rejestracja</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-
-          <div>
-            <div>
-              <h6>Obsługa klienta</h6>
-              <ul>
-                <li>
-                  <a href="/">Dostawa i płatność</a>
-                </li>
-                <li>
-                  <a href="/">Wymiana</a>
-                </li>
-                <li>
-                  <a href="/">Zwrot</a>
-                </li>
-                <li>
-                  <a href="/">Reklamacje</a>
-                </li>
-                <li>
-                  <a href="/">FAQ</a>
+                  <a
+                    href="/"
+                    className="footer__wrapper__list__item text-decoration-none "
+                  >
+                    Zasady gwarancji
+                  </a>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div>
-            <h6>Skontaktuj się z nami</h6>
-            <a href="tel:+48000000000">
+          <div className="footer__wrapper col-12 col-md-2  mobile-wrapper ">
+            <div className="footer__wrapper__border border-bottom border-1 ">
+              <h6 className="footer__wrapper__title fw-bold position-relative d-flex align-items-center justify-content-between">
+                Twoje konto
+                <img
+                  src={arrowdrop}
+                  alt="rozwiń menu"
+                  className="footer__wrapper__arrow"
+                />
+              </h6>
+              <ul className="footer__wrapper__list list-unstyled">
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Twoje konto
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Twoje zamówienia
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Logowanie
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Rejestracja
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="footer__wrapper col-12 col-md-2  mobile-wrapper ">
+            <div className="footer__wrapper__border border-bottom border-1 ">
+              <h6 className="footer__wrapper__title fw-bold position-relative d-flex align-items-center justify-content-between">
+                Obsługa klienta
+                <img
+                  src={arrowdrop}
+                  alt="rozwiń menu"
+                  className="footer__wrapper__arrow"
+                />
+              </h6>
+              <ul className="footer__wrapper__list list-unstyled">
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Dostawa i płatność
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Wymiana
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Zwrot
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    Reklamacje
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className=" footer__wrapper__list__item text-decoration-none "
+                  >
+                    FAQ
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="footer__wrapper footer__container col-12 col-md-4 row d-flex">
+            <h6 className="footer__wrapper__contact fw-semibold ">
+              Skontaktuj się z nami
+            </h6>
+            <a
+              href="tel:+48000000000"
+              className="footer__wrapper__tel fw-medium d-flex text-decoration-none"
+            >
               <img src={phone} alt="telefon" /> (+48) 000 000 000
             </a>
-            <a href="mailto:sklep@kontakt.com.pl">
+            <a
+              href="mailto:sklep@kontakt.com.pl"
+              className="footer__wrapper__email fw-medium d-flex text-decoration-none"
+            >
               <img src={email} alt="email" /> sklep@kontakt.com.pl
             </a>
-            <p>
+            <p className="footer__wrapper__desc ">
               Jesteśmy do Twojej dyspozycji od poniedziałku do piątku w
               godzinach 8:00 - 16:00
             </p>
