ab8ae3e73d90e39d91f16bfb0fde06984c489b5a
cmdspeed <cmdspeed@gmail.com>
2025-08-15 15:32:28 +0200

    feat:add slider view

--- src/components/SlickSlider/SlickSlider.jsx
+++ src/components/SlickSlider/SlickSlider.jsx
@@ -3,6 +3,8 @@ import $ from "jquery";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel";
+import leftArrow from "../../icon/leftArrow.svg";
+import rightArrow from "../../icon/rightArrow.svg";
 import "../styles/slickSlider.less";
 
 export const SlickSlider = () => {
@@ -10,7 +12,6 @@ export const SlickSlider = () => {
   const height = 520;
 
   useEffect(() => {
-    // ustalenie rozmiarów w zależności od typu urządzenia
     const isDesktop = window.innerWidth >= 768;
 
     const width = isDesktop ? 1400 : 358;
@@ -34,16 +35,20 @@ export const SlickSlider = () => {
     if (images.length > 0) {
       setTimeout(() => {
         const isPC = window.innerWidth >= 1000;
-        $(".slider").not(".slick-initialized").slick({
-          dots: true,
-          arrows: isPC,
-          infinite: true,
-          speed: 300,
-          slidesToShow: 1,
-          adaptiveHeight: true,
-          autoplay: true,
-          autoplaySpeed: 3000,
-        });
+        $(".slider")
+          .not(".slick-initialized")
+          .slick({
+            dots: true,
+            arrows: isPC,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            prevArrow: `<button type="button" class="slick-prev"><img src="${leftArrow}" alt="prev"/></button>`,
+            nextArrow: `<button type="button" class="slick-next"><img src="${rightArrow}" alt="next"/></button>`,
+          });
       }, 0);
     }
   }, [images]);
@@ -64,8 +69,12 @@ export const SlickSlider = () => {
               loading="lazy"
             />
             <div className="slider__overlay position-absolute">
-              <h2>{image.title}</h2>
-              <p className="fw-medium">{image.description}</p>
+              <div className="slider__overlay__text">
+                <h1 className="slider__overlay__title">{image.title}</h1>
+                <p className="slider__overlay__desc fw-medium">
+                  {image.description}
+                </p>
+              </div>
               <button className="bg-transparent border border-white rounded text-white fw-bold btn">
                 {image.buttonText}
               </button>
